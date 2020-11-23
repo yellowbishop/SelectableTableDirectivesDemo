47e6d3aa8eb6092912e47bc1c959af92b4dd2ba4
Varchar30 <adiezgon97@gmail.com>
2020-11-23 09:00:15 +0100

    Firs version
    First versión of demo ad-selectable-table-directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SelectableTableAPI } from 'ad-selectable-table-directives/public-api';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'SelectableTableDirectivesDemo';
+  title = 'SelectableTableDemo';
+  tableApi: SelectableTableAPI;
+  inputValue: number;
+  cellvalue = {
+    m: 'p',
+    c: 'h'
+  }
+  // columns: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+  // rows: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+  columns: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  rows: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  constructor() {
+
+  }
+  rowSelected($event) {
+    console.log('ROW SELECTED' + $event);
+  }
+  rowRemoved($event) {
+    console.log('ROW REMOVED' + $event);
+  }
+  columnSelected($event) {
+    console.log('COLUMN SELECTED' + $event);
+  }
+  columnRemoved($event) {
+    console.log('COLUMN REMOVED' + $event);
+  }
+  clear() {
+    this.tableApi.clearSelection();
+  }
+  selectRow(x) {
+    this.tableApi.selectRowByIndex(x)
+  }
+  tableReady($event) {
+    this.tableApi = $event;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { SelectableTableModule } from 'ad-selectable-table-directives';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,31 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    SelectableTableModule.forRoot([{
+      TABLE_ID: 'table1',
+      CELL_SELECTED_CLASSES: ['selected'],
+      CELL_SELECTABLE_CLASSES: ['selectable'],
+      ROWCELL_SELECTABLE_CLASSES: ['rowSelectable'],
+      ROWCELL_SELECTED_CLASSES: ['rowSelected'],
+      ROWCELL_PARCIAL_SELECTED_CLASSES: ['rowParcialSelected'],
+      COLUMNCELL_SELECTABLE_CLASSES: ['columnSelectable'],
+      COLUMNCELL_SELECTED_CLASSES: ['columnSelected'],
+      COLUMNCELL_PARCIAL_SELECTED_CLASSES: ['columnParcialSelected'],
+    },
+    {
+      TABLE_ID: '',
+      CELL_SELECTED_CLASSES: ['selected'],
+      CELL_SELECTABLE_CLASSES: ['selectable'],
+      ROWCELL_SELECTABLE_CLASSES: ['rowSelectable'],
+      ROWCELL_SELECTED_CLASSES: ['rowSelected'],
+      ROWCELL_PARCIAL_SELECTED_CLASSES: ['rowParcialSelected'],
+      COLUMNCELL_SELECTABLE_CLASSES: ['columnSelectable'],
+      COLUMNCELL_SELECTED_CLASSES: ['columnSelected'],
+      COLUMNCELL_PARCIAL_SELECTED_CLASSES: ['columnParcialSelected'],
+    }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
